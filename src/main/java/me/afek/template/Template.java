379d153663dec75532a06e40553e8f2a5188828d
Kefa Vova <41642071+afekvova@users.noreply.github.com>
2022-09-04 00:36:43 +0300

    add plugin annotation

--- src/main/java/me/afek/template/Template.java
+++ src/main/java/me/afek/template/Template.java
@@ -1,17 +1,24 @@
 package me.afek.template;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.Description;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.Website;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
+@Plugin(name = "Template", version = "1.0.0")
+@Description("A simple, human-friendly template")
+@Website("t.me/afekvova, github.com/afekvova, gitlab.com/afekvova, vk.com/afekvova")
+@Author("Afek")
 public final class Template extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
