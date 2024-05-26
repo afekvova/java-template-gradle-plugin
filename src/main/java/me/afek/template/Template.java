6b0c8ba9cdc662259fd17439e88684d60edbfb5b
Kefa Vova <kefavova24@gmail.com>
2024-05-27 00:35:35 +0300

    update paper version

--- src/main/java/me/afek/template/Template.java
+++ src/main/java/me/afek/template/Template.java
@@ -1,10 +1,14 @@
 package me.afek.template;
 
+import lombok.val;
 import me.afek.template.commands.TemplateCommand;
+import me.afek.template.listener.PlayerListener;
 import me.afek.template.settings.Settings;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.annotation.command.Command;
 import org.bukkit.plugin.java.annotation.command.Commands;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
 import org.bukkit.plugin.java.annotation.plugin.Description;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 import org.bukkit.plugin.java.annotation.plugin.Website;
@@ -12,10 +16,11 @@ import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
 import java.io.File;
 
-@Plugin(name = "Template", version = "1.0.0")
+@Plugin(name = "Template", version = "1.0.1")
 @Description("A simple, human-friendly template")
 @Website("t.me/afekvova, github.com/afekvova, gitlab.com/afekvova, vk.com/afekvova")
 @Author("Afek")
+@ApiVersion(ApiVersion.Target.v1_20)
 @Commands(@Command(name = "template"))
 public final class Template extends JavaPlugin {
 
@@ -23,6 +28,11 @@ public final class Template extends JavaPlugin {
     public void onEnable() {
         Settings.IMP.reload(new File(this.getDataFolder(), "config.yml"));
         this.getCommand("template").setExecutor(new TemplateCommand(this));
+    }
+
+    public void registerListeners() {
+        val pluginManager = Bukkit.getPluginManager();
+        pluginManager.registerEvents(new PlayerListener(), this);
     }
 
     @Override
